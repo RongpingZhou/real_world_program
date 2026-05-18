92affa059279a73eee2f6513dc713cdd8f986c9e
Rongping Zhou <rongping.zhou@hotmail.com>
2026-05-18 10:39:44 +1000

    clean the repo

--- rwrl_068_sensor_test_pyc.sh
+++ rwrl_068_sensor_test_pyc.sh
@@ -59,14 +59,15 @@ python code_068_dqn_agent_test_for_real_world_input_system.cpython-310.pyc \
     --gym-id "BreakoutNoFrameskip-v4" \
     --max-episode-steps 60000 \
     --model 1 \
-    --sensor 0 \
+    --sensor 1 \
     --play 0 \
     --display 1 \
     --training 0 \
     --test 1 \
     --n-episodes 100 \
     --cuda False \
-    --fps 300 \
+    --fps 120 \
     --zoom 1.5 \
     --seed 37 \
-    --plot 1
+    --plot 1 \
+    --crop 1
